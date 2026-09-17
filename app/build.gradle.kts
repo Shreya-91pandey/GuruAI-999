7df6aa306045690aeb1e013828e1d41a0ba007c5
Shreya-91pandey <sp327272@gmail.com>
2026-09-17 13:13:25 +0530

    Change applicationId in build.gradle.kts
    Updated applicationId to 'com.guruai.app2'.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.guruai.app"
+        applicationId = "com.guruai.app2"   // ← yahan change kiya
         minSdk = 26
         targetSdk = 35
         versionCode = 1
