6ab09d2d2db5ce6fb931185c873969730b136873
Shreya-91pandey <sp327272@gmail.com>
2026-09-17 12:52:27 +0530

    Add Modifier import to SettingsScreen.kt

--- app/src/main/java/com/guruai/app/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/guruai/app/ui/screens/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.guruai.app.ui.screens
 
+import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
