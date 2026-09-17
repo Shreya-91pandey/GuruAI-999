765a7e3b44a4169ff6efbd11878ebca3df9d602a
Shreya-91pandey <sp327272@gmail.com>
2026-09-17 12:51:32 +0530

    Add Modifier import to ChatScreen.kt

--- app/src/main/java/com/guruai/app/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/guruai/app/ui/screens/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.guruai.app.ui.screens
 
+import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
